1765548b92603f8d17402e413b6bb3de47373377
Jetmir <jetmir@apper.ch>
2019-08-28 10:23:46 +0200

    nav fix

--- client/components/nav.php
+++ client/components/nav.php
@@ -5,64 +5,54 @@
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarCollapse">
-            <ul id="nav-items" class="navbar-nav ml-auto">
+            <ul class="navbar-nav ml-auto">
+                <li id="index" class="nav-item">
+                    <a href="/" class="nav-link">Home</a>
+                </li>
+                <li id="diseases" class="nav-item">
+                    <a href="/diseases" class="nav-link">Diseases</a>
+                </li>
+                <li id="chat" class="nav-item">
+                    <a href="/chat" class="nav-link">Chat</a>
+                </li>
+                <?php
+                if (!isset($_SESSION['token'])) {
 
+                    ?>
+
+                <li id="login" class="nav-item">
+                    <a href="/login" class="nav-link">login</a>
+                </li>
+                <?php
+                } else {
+                    ?>
+                <li id="profile" class="nav-item">
+                    <a href="/profile" class="nav-link">Profile</a>
+                </li>
+
+                <?php
+                }
+                ?>
+                <li id="about" class="nav-item">
+                    <a href="/about" class="nav-link">About</a>
+                </li>
+                <?php
+                if (isset($_SESSION['token'])) {
+
+                    ?>
+                <li id="profile" class="nav-item">
+                    <a id="logout" href="logout" class="nav-link btn btn-danger">Logout</a>
+                </li>
+
+                <?php
+                }
+                ?>
             </ul>
         </div>
     </div>
 </nav>
 
 <script>
-    nav()
-
-    function nav() {
-        let token = localStorage.getItem('access_token')
-        if (token) {
-            $('#nav-items').append(`
-                <li id="index" class="nav-item">
-                    <a href="/" class="nav-link">Home</a>
-                </li>
-                <li id="diseases" class="nav-item">
-                    <a href="/diseases" class="nav-link">Diseases</a>
-                </li>
-                <li id="chat" class="nav-item">
-                    <a href="/chat" class="nav-link">Chat</a>
-                </li>
-                <li id="profile" class="nav-item">
-                    <a href="/profile" class="nav-link">Profile</a>
-                </li>
-                <li id="about" class="nav-item">
-                    <a href="/about" class="nav-link">About</a>
-                </li>
-                <li id="profile" class="nav-item">
-                    <a id="logout" href="logout" class="nav-link btn btn-danger">Logout</a>
-                </li>
-            `)
-        } else {
-            $('#nav-items').append(`
-                <li id="index" class="nav-item">
-                    <a href="/" class="nav-link">Home</a>
-                </li>
-                <li id="diseases" class="nav-item">
-                    <a href="/diseases" class="nav-link">Diseases</a>
-                </li>
-                <li id="chat" class="nav-item">
-                    <a href="/chat" class="nav-link">Chat</a>
-                </li>
-                <li id="login" class="nav-item">
-                    <a href="/login" class="nav-link">login</a>
-                </li>
-                <li id="about" class="nav-item">
-                    <a href="/about" class="nav-link">About</a>
-                </li>
-    
-            `)
-        }
-        path()
-    }
-
-
-
     $('#logout').click(e => {
         e.preventDefault();
         $.post('../helpers/authorization.php', {
@@ -74,27 +64,25 @@
 
 
 
-
-    function path() {
-        let path = window.location.pathname.split('/');
-        if (path.includes('index')) {
-            document.getElementById("index").setAttribute('class', 'active')
-        } else if (path.includes('diseases')) {
-            document.getElementById("diseases").setAttribute('class', 'active')
-        } else if (path.includes('chat')) {
-            document.getElementById("chat").setAttribute('class', 'active')
-        } else if (path.includes('login')) {
-            document.getElementById("login").setAttribute('class', 'active')
-        } else if (path.includes('register')) {
-            document.getElementById("login").setAttribute('class', 'active')
-        } else if (path.includes('about')) {
-            document.getElementById("about").setAttribute('class', 'active')
-        } else if (path.includes('profile')) {
-            document.getElementById("profile").setAttribute('class', 'active')
-        } else if (path.includes('patient')) {
-            document.getElementById("profile").setAttribute('class', 'active')
-        } else {
-            document.getElementById("index").setAttribute('class', 'active')
-        }
+    let path = window.location.pathname.split('/');
+    if (path.includes('index')) {
+        document.getElementById("index").setAttribute('class', 'active')
+    } else if (path.includes('diseases')) {
+        document.getElementById("diseases").setAttribute('class', 'active')
+    } else if (path.includes('chat')) {
+        document.getElementById("chat").setAttribute('class', 'active')
+    } else if (path.includes('login')) {
+        document.getElementById("login").setAttribute('class', 'active')
+    } else if (path.includes('register')) {
+        document.getElementById("login").setAttribute('class', 'active')
+    } else if (path.includes('about')) {
+        document.getElementById("about").setAttribute('class', 'active')
+    } else if (path.includes('profile')) {
+        document.getElementById("profile").setAttribute('class', 'active')
+    } else if (path.includes('patient')) {
+        document.getElementById("profile").setAttribute('class', 'active')
+    } else {
+        document.getElementById("index").setAttribute('class', 'active')
     }
+
 </script>
